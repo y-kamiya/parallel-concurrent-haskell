9c5dc5eebee9524d6ae6d401884a977fecdf906d
Yuji Kamiya <y.kamiya0@gmail.com>
2015-02-20 09:42:57 +0900

    add async

--- src/Async/Main.hs
+++ src/Async/Main.hs
@@ -2,6 +2,19 @@ import Control.Concurrent
 import Network.HTTP
 import System.Environment
 
+data Async a = Async (MVar a)
+
+async :: IO a -> IO (Async a)
+async action = do
+  mvar <- newEmptyMvar
+  forkIO $ do
+    r <- action
+    putMVar mvar r
+  return (Async mvar)
+
+wait :: Async a -> IO a
+wait (Async mvar) = readMVar mvar
+
 getURL :: String -> IO String
 getURL url = simpleHTTP (getRequest url) >>= getResponseBody
 
@@ -10,6 +23,7 @@ main = do
   [arg] <- getArgs
   case arg of
     "1" -> geturl1
+    "2" -> geturl2
     _ -> print "no such arguments"
 
 geturl1 :: IO ()
@@ -26,3 +40,10 @@ geturl1 = do
   r2' <- takeMVar m2
   print (length r1', length r2')
 
+geturl2 :: IO ()
+geturl2 = do
+  a1 <- async $ getURL "http://hackage.haskell.org/package/HTTP-4000.0.5/docs/Network-HTTP.html"
+  a2 <- async $ getURL "http://hackage.haskell.org/package/HTTP-4000.0.6/docs/Network-HTTP.html"
+  r1 <- wait a1
+  r2 <- wait a2
+  print (length r1, length r2)
